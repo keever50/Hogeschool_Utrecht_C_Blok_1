dee7026b0a36a3c6916332f4f700b2c74c989cfb
Kevin Witteveen <kevin.witteveen@student.hu.nl>
2023-10-05 09:26:29 +0200

    Switch Case

--- SwitchCase/main.c
+++ SwitchCase/main.c
@@ -1,5 +1,10 @@
+/*
+Naam: Kevin Witteveen
+Datum: 05/10/2023
+Programma: SwitchCase
+Geschreven in: Visual code (GCC C99)
+*/
 #include <stdio.h>
-
 
 void print_som(int tafel)
 {
@@ -12,6 +17,24 @@ void print_som(int tafel)
     printf("som van tafel %d: %d\n", tafel, som);
 }
 
+void print_tafel(int tafel)
+{
+    int n;
+    for(n=1;n<=10;n++)
+    {
+        printf("%d*%d=%d\n", n, tafel, n*tafel);
+    }
+}
+
+void print_mod(int tafel)
+{
+    int n;
+    for(n=1;n<=10;n++)
+    {
+        printf("%d*%d%%3=%d\n",n,tafel,(n*tafel)%3);
+    }
+}
+
 int main()
 {
     int input;
@@ -19,20 +42,22 @@ int main()
 
     switch(input)
     {
-        case 1:
+        case 1: /*Lege cases want zonder break negeert hij andere cases, waardoor hij werkt als een 'or' functie*/
         case 2:
             print_som(input);
             break;
         case 3:
         case 4:
         case 5:
-
-        break;
+            print_tafel(input);
+            break;
         case 6:
         case 7:
         case 8:
-        break;
+            print_mod(input);
+            break;
         default:
-        break;
+            printf("het 1e couplet van het Wilhelmus");
+            break;
     }
 }
